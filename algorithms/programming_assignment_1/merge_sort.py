aba16123b1d558e44187ae820fcf15f3ea5090b4
Loide Mara Verdes <mara.verdes@gmail.com>
2018-07-12 21:45:38 -0400

    Update to count inversions when sorting
    Inversions are pair of numbers, in a disordered list, where the
    larger of the two numbers is to the left of smaller number.
    Example: [1,3,5,2,4,6]
    There are 3 inversions: (3,2), (5,2), (4,2)

--- algorithms/programming_assignment_1/merge_sort.py
+++ algorithms/programming_assignment_1/merge_sort.py
@@ -1,6 +1,34 @@
-def merge(arr1, arr2):
+"""
+Download the following text file:
+    IntegerArray.txt
+
+This file contains all of the 100,000 integers between 1 and 100,000 (inclusive)
+in some order, with no integer repeated.
+
+Your task is to compute the number of inversions in the file given, where the ith
+row of the file indicates the ith entry of an array.
+
+Because of the large size of this array, you should implement the fast
+divide-and-conquer algorithm covered in the video lectures.
+
+The numeric answer for the given input file should be typed in the space below.
+
+So if your answer is 1198233847, then just type 1198233847 in the space provided
+without any space / commas / any other punctuation marks. You can make up to 5
+attempts, and we'll use the best one for grading.
+
+(We do not require you to submit your code, so feel free to use any programming
+language you want --- just type the final numeric answer in the following space
+.)
+
+[TIP: before submitting, first test the correctness of your program on some
+small test files or your own devising. Then post your best test cases to the
+discussion forums to help your fellow students!]
+"""
+
+def merge(arr1, arr2, inversions):
     if not len(arr1) or not len(arr2):
-        return arr1 or arr2
+        return arr1,0 or arr2,0
 
     arr_result = []
     i,j = 0, 0
@@ -12,23 +40,25 @@ def merge(arr1, arr2):
         else:
             arr_result.append(arr2[j])
             j+=1
+            inversions+=(len(arr1) - i)
 
-        if (i == len(arr1) or j == len(arr2)):
-            arr_result.extend(arr1[i:] or arr2[j:])
+    if (i == len(arr1) or j == len(arr2)):
+        arr_result.extend(arr1[i:] or arr2[j:])
 
-    return arr_result
+    return arr_result, inversions
 
 def merge_sort(arr):
     if (len(arr) < 2):
-        return arr
+        return arr,0
 
     middle = len(arr)/2
     arr1 = arr[:int(middle)]
     arr2 = arr[int(middle):]
 
-    arr1 = merge_sort(arr[:int(middle)])
-    arr2 = merge_sort(arr[int(middle):])
-    return merge(arr1,arr2)
+    arr1,arr1_inversions = merge_sort(arr[:int(middle)])
+    arr2,arr2_inversions = merge_sort(arr[int(middle):])
+    inversions = 0 + arr1_inversions + arr2_inversions
+    return merge(arr1,arr2,inversions)
 
 def loadData(arr):
     """Load data from input file """
@@ -42,9 +72,13 @@ def main():
     arr = []
     loadData(arr)
 
-    arr = merge_sort(arr)
-    for i in arr:
-        print(i)
+    arr,n_inversions = merge_sort(arr)
+
+    """Print sorted array """
+    #for i in arr:
+    #    print(i)
+
+    print("Total inversions: " + str(n_inversions))
 
 if __name__ == "__main__":
     main()
